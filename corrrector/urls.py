b0e948d1ea352397758db4258f45d28b8adb8073
Kyle Maxwell <56495646+IAmSweeps@users.noreply.github.com>
2019-12-07 15:01:05 -0600

    Update urls.py

--- corrrector/urls.py
+++ corrrector/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authentication.urls')),
+    path('note/', include('authentication.urls')),
+    
 ]
